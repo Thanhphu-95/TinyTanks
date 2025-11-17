91fc6b63a4ae3151e8cdde983dc44d46330abc75
Thanhphu-95 <thanhphu1505@gmail.com>
2025-11-17 16:20:50 +0700

    fix: đạn trúng hide wall

--- Assets/Scripts/BarrelRotator.cs
+++ Assets/Scripts/BarrelRotator.cs
@@ -2,10 +2,10 @@
 
 public class BarrelRotator : MonoBehaviour
 {
-    public Transform barrel;    // Kéo Barrel vào đây
+    public Transform barrel;    
     public float rotateSpeed = 20f;
-    public float minAngle;   // hạ nòng tối đa
-    public float maxAngle = 30f;   // nâng nòng tối đa
+    public float minAngle;   
+    public float maxAngle = 30f;   
 
     public LineRenderer line;
     public float laserLength = 20f;
@@ -14,34 +14,25 @@ public class BarrelRotator : MonoBehaviour
 
     void Update()
     {
-        // Scroll mouse (giá trị thường là -1, 0, 1)
+        
         float scroll = Input.GetAxis("Mouse ScrollWheel");
 
         if (scroll != 0f)
         {
-            // tăng/giảm góc theo chiều scroll
+
             currentAngle -= scroll * rotateSpeed;
-
-            // giới hạn
             currentAngle = Mathf.Clamp(currentAngle, minAngle, maxAngle);
-
-            // áp dụng
             barrel.localRotation = Quaternion.Euler(currentAngle, 0, 0);
         }
-
         Line();
     }
 
 
     public void Line()
     {
-        // Điểm đầu nên lấy từ đầu nòng súng (firePoint nếu có)
         Vector3 start = barrel.position;
 
-        // HƯỚNG LASER phải lấy từ barrel!
         Vector3 direction = barrel.forward;
-        // hoặc barrel.right nếu mô hình của bạn bắn theo trục X
-
         Vector3 end = start + direction * laserLength;
 
         line.SetPosition(0, start);
